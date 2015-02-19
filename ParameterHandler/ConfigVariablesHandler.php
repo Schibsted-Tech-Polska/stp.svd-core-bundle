f9f3265a3778665760174bbd5df9b635b95a13fe
Piotr Kedra <piotr.kedra@schibsted.pl>
2015-02-19 13:10:43 +0100

    Add ParameterHandler class

---
+++ ParameterHandler/ConfigVariablesHandler.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Svd\CoreBundle\ParameterHandler;
+
+use Composer\Script\Event;
+
+/**
+ * ParameterHandler
+ */
+class ConfigVariablesHandler
+{
+    /**
+     * Populate environment
+     *
+     * @param Event $event event
+     */
+    public static function populateEnvironment(Event $event)
+    {
+        $variable = 'DATABASE_URL';
+
+        $databaseUrl = getenv($variable);
+        if (!empty($databaseUrl)) {
+            $items = parse_url($databaseUrl);
+            if (!empty($items)) {
+                putenv('DOCTRINE__DATABASE_HOST=' . $items['host']);
+                putenv('DOCTRINE__DATABASE_DBNAME=' . substr($items['path'], 1));
+                putenv('DOCTRINE__DATABASE_PASSWORD=' . $items['pass']);
+                putenv('DOCTRINE__DATABASE_PORT=' . $items['port']);
+                putenv('DOCTRINE__DATABASE_USER=' . $items['user']);
+
+                $io = $event->getIO();
+                $io->write("Parameters from variable '" . $variable . "' has been updated.");
+            }
+        }
+    }
+}
