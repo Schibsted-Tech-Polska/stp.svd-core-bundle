694ad3e4f72bd1c1a8952a6b72bb4942ac369060
Kamil Wojciech <kamil.wojciech@schibsted.pl>
2015-09-25 10:56:29 +0200

    add deleted at trait

---
+++ Model/DeletedAtTrait.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Svd\CoreBundle\Model;
+
+use DateTime;
+
+/**
+ * Model
+ */
+trait DeletedAtTrait
+{
+    /**
+     * @var DateTime
+     */
+    protected $deletedAt;
+
+    /**
+     * Set deleted at
+     *
+     * @param DateTime $deletedAt deleted at
+     *
+     * @return self
+     */
+    public function setUpdatedAt(DateTime $deletedAt)
+    {
+        $this->deletedAt = $deletedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get deleted at
+     *
+     * @return DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->deletedAt;
+    }
+}
